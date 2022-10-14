f896367403290fb4aaab11aab304223fd263f224
Tatyana <tatyana.bazh17@gmail.com>
2022-10-14 17:27:05 +0400

    продолжаю решать задачу 29

--- 29array/Program.cs
+++ 29array/Program.cs
@@ -2,20 +2,55 @@
 // элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
-Console.WriteLine("1, 2, 5, 7, 19");
+Console.WriteLine("Введите числа через запятую: ");
+string msg = Console.ReadLine();
 
-string textNumbers = "1, 2, 5, 7, 19";
-string Text(string textNumbers, char comma, char interval)
+Console.WriteLine("переводим строку в массив: ");
+int[] array = new int [8];
+
+void PrintArray(String[] collection)
 {
-    string result = string.Empty;
-    int length = textNumbers.Length;
-    for (int index = 1; index = length; index++)
+
+    int count = collection.Length;
+    for (int position = 0; position < count; position++)
     {
-        if (textNumbers [index] == comma || textNumbers [index] == interval) 
-        result = result + $"{textNumbers[index+1]}";
-        else result = result + $"{textNumbers[index]}";
-    }     
-    return result;
+        Console.Write($"{collection[position]} ");
+    }
 }
-string newTextNumbers = Text(textNumbers, ',', ' ');
-Console.WriteLine(newTextNumbers);
+// PrintArray(array);
+
+int SpaceIndex = 0;
+int CommaIndex = 0;
+
+
+string str  = "31, 24, 5, 7, 19";
+
+string s = ""; // для чисел
+for () { // цикл по строке поэлементно
+    string el = str[i];
+    if (el.Equals(",") || el.Equals(" ")) {
+     // todo 1 добавить el в массив
+    // обнулить el
+    el = "";
+    } else {
+        s = s + el;
+    }
+}
+
+
+
+
+// int length = str.IndexOf(", ");
+// Console.WriteLine(str.Substring(0, length));
+// str = str.Substring(str.IndexOf(", ") + 2);
+// Console.WriteLine(str);
+String[] sa = str.Split();
+PrintArray(sa);
+
+// char[] arrayNew = msg.ToCharArray();
+// int length = arrayNew.Length;
+// for(int index = 0; index < length; index++)
+// {
+
+// } 
+// Console.WriteLine(arrayNew);
